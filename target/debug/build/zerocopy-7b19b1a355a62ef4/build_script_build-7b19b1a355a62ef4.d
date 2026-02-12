dbdcfe826a4b0d2ba9c7d209ff78668a774c5163
zhdidoks123 <mshustov131213@gmail.com>
2026-02-12 22:54:24 +0300

    Commit 5.1: Nucked the 	arget/ directory

--- target/debug/build/zerocopy-7b19b1a355a62ef4/build_script_build-7b19b1a355a62ef4.d
+++
@@ -1,5 +0,0 @@
-C:\Users\katef\koshka2-rs\target\debug\build\zerocopy-7b19b1a355a62ef4\build_script_build-7b19b1a355a62ef4.d: C:\Users\katef\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zerocopy-0.8.31\build.rs
-
-C:\Users\katef\koshka2-rs\target\debug\build\zerocopy-7b19b1a355a62ef4\build_script_build-7b19b1a355a62ef4.exe: C:\Users\katef\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zerocopy-0.8.31\build.rs
-
-C:\Users\katef\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zerocopy-0.8.31\build.rs:
